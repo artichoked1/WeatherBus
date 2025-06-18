80d318a5a01f1a2eb57eb93ca20d5bdd7b17f6f5
artichoked1 <41409702+artichoked1@users.noreply.github.com>
2025-06-18 16:29:14 +0930

    change formatting

--- PayloadBuilder.c
+++ PayloadBuilder.c
@@ -24,6 +24,32 @@ sensorbus_error_t pb_add_tlv(payload_builder_t* pb, uint8_t type, uint8_t index,
     return SENSORBUS_OK;
 }
 
+sensorbus_error_t pb_decode_tlv(const uint8_t* buf, uint8_t buf_len, sensorbus_sensor_t* out, size_t* out_count) {
+    size_t count = 0;
+    uint8_t pos = 0;
+    
+    while (pos + 3 <= buf_len && count < SENSORBUS_MAX_TLVS) {
+    uint8_t t   = buf[pos++];
+    uint8_t i   = buf[pos++];
+    uint8_t L   = buf[pos++];
+    if (pos + L > buf_len) {
+    return SENSORBUS_ERR_FORMAT;
+    }
+    out[count].type  = t;
+    out[count].index = i;
+    out[count].len   = L;
+    out[count].value = &buf[pos];
+    pos += L;
+    count++;
+    }
+    
+    if (pos != buf_len) {
+    return SENSORBUS_ERR_FORMAT;
+    }
+    *out_count = count;
+    return SENSORBUS_OK;
+    }
+
 sensorbus_error_t pb_add_float(payload_builder_t* pb, uint8_t type, uint8_t index, float value) {
     return pb_add_tlv(pb, type, index, &value, sizeof(value));
 }
@@ -31,32 +57,6 @@ sensorbus_error_t pb_add_float(payload_builder_t* pb, uint8_t type, uint8_t inde
 sensorbus_error_t pb_add_error(payload_builder_t* pb, uint8_t type, uint8_t index) {
     const uint8_t err_val[4] = {0xFF, 0xFF, 0xFF, 0xFF};
     return pb_add_tlv(pb, type, index, err_val, sizeof(err_val));
-}
-
-sensorbus_error_t pb_decode_tlv(const uint8_t* buf, uint8_t buf_len, sensorbus_sensor_t* out, size_t* out_count) {
-size_t count = 0;
-uint8_t pos = 0;
-
-while (pos + 3 <= buf_len && count < SENSORBUS_MAX_TLVS) {
-uint8_t t   = buf[pos++];
-uint8_t i   = buf[pos++];
-uint8_t L   = buf[pos++];
-if (pos + L > buf_len) {
-return SENSORBUS_ERR_FORMAT;
-}
-out[count].type  = t;
-out[count].index = i;
-out[count].len   = L;
-out[count].value = &buf[pos];
-pos += L;
-count++;
-}
-
-if (pos != buf_len) {
-return SENSORBUS_ERR_FORMAT;
-}
-*out_count = count;
-return SENSORBUS_OK;
 }
 
 sensorbus_error_t pb_add_query(payload_builder_t *pb, sensorbus_sensor_t *sensor){
