4b5caa2f66969e070baec87f3e65fc1f47b41bc4
artichoked1 <41409702+artichoked1@users.noreply.github.com>
2025-08-08 22:38:55 +0930

    disable arduino hal and add comment

--- src/hal/arduino_hal.cpp
+++ src/hal/arduino_hal.cpp
@@ -1,3 +1,33 @@
+/*
+ * arduino_hal.cpp
+ *
+ * This file is part of the WeatherBus library.
+ * Copyright (c) 2025 Nikolai Patrick
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * This is an example HAL designed to be built on the application level, not as a library.
+ * Since Arduino/PlatformIO just globs all .cpp files, I have prevented it from being compiled
+ * with the ENABLED define.
+ * 
+ * If you want to use this HAL, paste it into your project have it built there.
+ * This is so you can use the HAL without having to modify the library itself, and also to access
+ * the uart device directly for things like putting the trnasceiver into shutdown mode.
+ */
+// #define ENABLED
+
+#ifdef ENABLED
 #if defined(__AVR__)
 
 #include "weatherbus.h"
@@ -51,4 +81,5 @@ uint64_t sensorbus_hal_get_time_ms() {
     return millis();
 }
 
+#endif
 #endif
